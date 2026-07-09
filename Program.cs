c35e47e72adca072b4e0384682a277303b633bba
Reinaldo/python <reinaldo_997@hotmail.com>
2026-07-09 00:38:36 -0300

    Criando variaveis

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
 ﻿using Estudos_C__.net.Models;
 
-Pessoa pessoa1 = new Pessoa();
 
-pessoa1.Nome = "Reinaldo";
-pessoa1.Idade = 29;
-pessoa1.Apresentar();
+string apresentacao = "Olá, seja bem vindo!";
+
+int quantidade = 1;
+
+double altura = 1.80;
+
+decimal preco = 1.80m;
+
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variavel quantidade: " + quantidade);
+Console.WriteLine("Valor da variavel altura: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variavel preco: " + preco);
+Console.WriteLine("Valor da variavel condicao: " + condicao);
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Reinaldo";
+// pessoa1.Idade = 29;
+// pessoa1.Apresentar();
