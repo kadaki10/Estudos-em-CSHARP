ac2a803f63a09cc39cdfb15a465b8ca1873886f4
Reinaldo/python <reinaldo_997@hotmail.com>
2026-07-09 18:00:02 -0300

    DateTime

--- Program.cs
+++ Program.cs
@@ -1,21 +1,40 @@
-﻿using Estudos_C__.net.Models;
+﻿using System.Data.Common;
+using Estudos_C__.net.Models;
 
 
-string apresentacao = "Olá, seja bem vindo!";
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:MM"));
 
-int quantidade = 1;
 
-double altura = 1.80;
 
-decimal preco = 1.80m;
 
-bool condicao = true;
 
-Console.WriteLine(apresentacao);
-Console.WriteLine("Valor da variavel quantidade: " + quantidade);
-Console.WriteLine("Valor da variavel altura: " + altura.ToString("0.00"));
-Console.WriteLine("Valor da variavel preco: " + preco);
-Console.WriteLine("Valor da variavel condicao: " + condicao);
+
+
+
+
+
+
+
+
+
+
+
+// string apresentacao = "Olá, seja bem vindo!";
+
+// int quantidade = 1;
+
+// double altura = 1.80;
+
+// decimal preco = 1.80m;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variavel quantidade: " + quantidade);
+// Console.WriteLine("Valor da variavel altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variavel preco: " + preco);
+// Console.WriteLine("Valor da variavel condicao: " + condicao);
 
 // Pessoa pessoa1 = new Pessoa();
 
