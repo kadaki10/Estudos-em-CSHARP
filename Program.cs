23d386f2378ee36cd3b76191b0f0342289d8b6fc
Reinaldo/python <reinaldo_997@hotmail.com>
2026-07-13 00:26:07 -0300

    Aprendendo diferenças de arrays

--- Program.cs
+++ Program.cs
@@ -2,17 +2,30 @@
 using Estudos_C__.net.Models;
 
 
-int[] arrayInteiros = new int[3];
+int[] arrayInteiros = new int[4];
 
 arrayInteiros[0] = 72;
 arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
-// arrayInteiros[3] = 1;
+arrayInteiros[3] = 1;
 
+Console.WriteLine("Percorrendo o ARRAY com o FOR");
 for(int contador = 0; contador < arrayInteiros.Length; contador++)
 {
     Console.WriteLine($"Posição N° {contador} - {arrayInteiros[contador]}");
 }
+
+Console.WriteLine("Percorrendo o ARRAY com o FOREACH");
+
+int contadorForeach = 0;
+foreach(int valor in arrayInteiros)
+{
+    Console.WriteLine($"Posição N° {contadorForeach} - {valor}");
+    contadorForeach++;
+}
+
+    
+
 
 
 
