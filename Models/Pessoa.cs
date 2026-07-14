b78bf5f2daeb687bf6e2c89af15b84882c300278
Reinaldo/python <reinaldo_997@hotmail.com>
2026-07-13 22:43:04 -0300

    Introdução propriedades

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -8,11 +8,6 @@ namespace Estudos_C__.net.Models
     public class Pessoa
     {
         public string Nome { get; set; }
-        public int Idade { get; set; }
-
-        public void Apresentar()
-        {
-            Console.WriteLine($"Olá, meu nome é {Nome} e tenho {Idade} anos.");
-        }
+        public string Idade { get; set; }
     }
 }
