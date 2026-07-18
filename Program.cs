e72a3104a561830690f31669fc4f3573af9996b4
Reinaldo/python <reinaldo_997@hotmail.com>
2026-07-17 21:51:59 -0300

    Introdução ao Throw

--- Program.cs
+++ Program.cs
@@ -2,71 +2,78 @@
 using Estudos_C__.net.Models;
 using System.Globalization;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
 //Console.WriteLine("Chegou até aqui");
 
 
-try
-{
-    string[] linhas = File.ReadAllLines("Arquivos/d/arquivo_Leitura.txt");
+// try
+// {
+//     string[] linhas = File.ReadAllLines("Arquivos/d/arquivo_Leitura.txt");
 
-    foreach(string linha in linhas)
-    {
-        Console.WriteLine(linha);
-    }
-} 
-catch(FileNotFoundException ex)
-{
-    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
-}
-catch(DirectoryNotFoundException ex)
-{
-    Console.WriteLine("Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado. " + ex.Message);
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Ocorreu uma exceção generica. {ex.Message}");
-}
-
-
-Console.WriteLine("Chegou até aqui");
+//     foreach(string linha in linhas)
+//     {
+//         Console.WriteLine(linha);
+//     }
+// } 
+// catch(FileNotFoundException ex)
+// {
+//     Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
+// }
+// catch(DirectoryNotFoundException ex)
+// {
+//     Console.WriteLine("Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado")
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine($"Ocorreu uma exceção generica. {ex.Message}");
+// }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Console.WriteLine("Chegou até aqui");
 
 
 
